2b1d3ac971ff3433d4218b7fab4f02ac5b6411c3
CaetanoY <125322239+CaetanoY@users.noreply.github.com>
2023-11-28 16:13:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("Automação residencial").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Residencial.html";
+});
+
+document.getElementById("Automação industrial").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Industrial.html";
+});
+
+document.getElementById("Automação na saude").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Saude.html";
+});
+
+document.getElementById("Inicio").addEventListener("click", function() {
+    // Redireciona para outra página HTML
+    window.location.href = "Index.html";
+});
+
+function goBack() {
+    window.location.href = "index.html"; // Substitua "index.html" pelo caminho da sua página principal
+  }
